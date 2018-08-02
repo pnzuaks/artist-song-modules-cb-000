9de237da758dc519126a0e8648d1bb38fdc7e7a4
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-02 19:05:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ require 'pry'
 
 class Song
   extend Memorable
+  extend Findable
   include Paramable
   attr_accessor :name
   attr_reader :artist
@@ -13,9 +14,9 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def artist=(artist)
     @artist = artist
